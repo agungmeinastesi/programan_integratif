3cd9f8a48a1af4415a20ba79a677ab112c091bce
agungmeinastesi <agungmeinastesi@gmail.com>
2013-12-13 16:39:36 +0700

    coba lagii

---
+++ index.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+<title>Web service</title>
+Agung Meinastesi Caesar
+
+</br>
+</br>
+</br>
+Versi xml
+<a href="example.xml">xml</a> 
+</br>
+</br>
+</br>
+Versi Json
+
+<a href="xml2json">json</a>
+
+</br>
+</br>
+
+</br>
+</br>
+
+Tugas 3
+
+
+</br>
+</br>
+
+<a href ="/protiftest/programan_integratif"/>akses repo semua temen STI</a></br> </br>
+</br>
+</br>
+<a href ="xmlphpsatukelas.php"</a>xml satu kelas digabung pakai php</a></br>
+</br>
+</br>
+Akses punya teman satu kelas tapi sendiri-sendiri
+</br>
+</br>
+
+
+<a href ="/programan_integratif/18211010-18211035/progin.xml">akses punya orang</a> </br>	
+<a href ="/programan_integratif/18211014-dan-18211029/datasiswa.xml">akses punya orang</a> </br>	
+	
+<a href ="/programan_integratif/BintangAdinandra/menu.xml">akses punya orang</a> </br>	
+<a href ="/programan_integratif/II3160-18211003-18211050/Menu.xml">akses punya orang</a> </br>	
+<a href ="/programan_integratif/II3160-Pemograman-Integratif/menu.xml">akses punya orang</a> </br>	
+<a href ="/programan_integratif/II3160--Pemrograman-Integratif-/DaftarIdol.xml">akses punya orang</a> </br>	
+<a href ="/programan_integratif/II3160-Progin-18211002-18211033/dbxml.xml">akses punya orang</a> </br>	
+<a href ="/programan_integratif/II3160-Tugas1-Tugas2/csvoutput.xml">akses punya orang</a> </br>	
+
+<a href ="/programan_integratif/pemrograman_integratif/file1.xml">akses punya orang</a> </br>	
+<a href ="/programan_integratif/progin/contoh.xml">akses punya orang</a> </br>	
+<a href ="/programan_integratif/progin-raosanfady/data.xml">akses punya orang</a> </br>	
+<a href ="/programan_integratif/testPHP2/test.xml">akses punya orang</a> </br>	
+<a href ="/programan_integratif/tugas-2-pemrograman-integratif/data3.xml">akses punya orang</a> </br>	
+<a href ="/programan_integratif/Web-Service/MyData.xml">akses punya orang</a> </br>	
+
+
+
